e7c6a4856b40a3acbea4b8f30c8d49335c4217cf
iz00 <isadoracontsost@gmail.com>
2024-03-27 23:02:02 -0300

    Add functionality to log user out

--- birthday_hub/views.py
+++ birthday_hub/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
 
 from .forms import LoginForm, RegisterForm
@@ -28,6 +28,11 @@ def login_view(request):
     if user:
         login(request, user)
         return redirect("birthday_hub:index")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("birthday_hub:login")
 
 
 def register(request):
